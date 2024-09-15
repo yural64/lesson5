20d87ebd1d225dfb5e47a3b6d902e9ab9cb3fe16
yural64 <yural64@gmail.com>
2024-09-15 11:37:14 +0300

    dz1 covpleted

--- taskmanager.py
+++ taskmanager.py
@@ -3,7 +3,7 @@ class Task():
         self.tasks = []
 
     def add_task(self, description, deadline):
-        self.task.append({"description": description, "deadline": deadline,
+        self.tasks.append({"description": description, "deadline": deadline,
                           "status": "Не выполнено"})
 
     def complete_tasks(self, description):
@@ -12,13 +12,24 @@ class Task():
                 task["status"] = "Выполнено"
                 print(f"Задача {description} выполнена")
             else:
-                print(f"Задача {description} не найдена")
+                print(f"Задача {description} не найдена в списке текущих задач.")
 
     def show_tasks(self):
-        print("Список задач")
+        print()
+        print("Список задач - срок:")
+        print("--------------------")
         for task in self.tasks:
             if task["status"] == "Не выполнено":
                 print(f"{task["description"]} - {task["deadline"]}")
 
 # Выполнение задачи
 t = Task()
+t.add_task("Выполнить ДЗ", "14.09.2024")
+t.add_task("Прочитать книгу", "30.09.2024")
+t.add_task("Починить холодильник", "19.09.2024")
+
+t.show_tasks()
+
+t.complete_tasks("Выполнить ДЗ")
+
+t.show_tasks()
